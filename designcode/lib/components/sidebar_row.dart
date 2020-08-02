94965cf112c1d5c664b3cf0c936b3cd9db52b173
Sai Kambampati <ssv1065@gmail.com>
2020-08-02 13:28:04 -0700

    4. Classes in Dart

---
+++ designcode/lib/components/sidebar_row.dart
@@ -0,0 +1,35 @@
+import 'package:designcode/model/sidebar.dart';
+import 'package:flutter/material.dart';
+
+class SidebarRow extends StatelessWidget {
+  SidebarRow({@required this.item});
+
+  final SidebarItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+            width: 42.0,
+            height: 42.0,
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(14.0),
+                gradient: item.background),
+            child: item.icon),
+        SizedBox(width: 12),
+        Container(
+          child: Text(
+            item.title,
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w800,
+              color: Color(0xFF242629),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- designcode/lib/main.dart
+++ designcode/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:designcode/components/sidebar_row.dart';
+import 'package:designcode/model/sidebar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,42 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Row(
-            children: [
-              Container(
-                width: 42.0,
-                height: 42.0,
-                padding: EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(14.0),
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Color(0xFF00AEFF),
-                      Color(0xFF0076FF),
-                    ],
-                  ),
-                ),
-                child: Icon(
-                  Icons.home,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(width: 12),
-              Container(
-                child: Text(
-                  "Home",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w800,
-                    color: Color(0xFF242629),
-                  ),
-                ),
-              ),
-            ],
+          child: SidebarRow(
+            item: sidebarItem[3],
           ),
         ),
       ),
