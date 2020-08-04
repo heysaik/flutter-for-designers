60e9547d0869918e25f008c2cb4951e9d6c3e4a7
Sai Kambampati <ssv1065@gmail.com>
2020-08-04 15:49:51 -0700

    7. Detecting Tap Gestures

--- designcode/lib/main.dart
+++ designcode/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:designcode/constants.dart';
 import 'package:designcode/model/course.dart';
 import 'package:flutter/material.dart';
 import 'components/cards/recent_course_card.dart';
@@ -12,12 +13,130 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SafeArea(
-          child: Container(
-            child: RecentCourseCard(
-              course: recentCourses[2],
+        body: Container(
+          color: kBackgroundColor,
+          child: SafeArea(
+            child: Column(
+              children: [
+                HomeScreenNavBar(),
+                RecentCourseCard(
+                  course: recentCourses[2],
+                ),
+              ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreenNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SidebarButton(),
+          SearchFieldWidget(),
+          Icon(
+            Icons.notifications,
+            color: kPrimaryLabelColor,
+          ),
+          SizedBox(
+            width: 16.0,
+          ),
+          CircleAvatar(
+            radius: 18.0,
+            backgroundImage: AssetImage('asset/images/profile.jpg'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchFieldWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.only(left: 12.0, right: 33.0),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(14.0),
+              boxShadow: [
+                BoxShadow(
+                  color: kShadowColor,
+                  offset: Offset(0, 12),
+                  blurRadius: 16.0,
+                )
+              ]),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextField(
+              cursorColor: kPrimaryLabelColor,
+              decoration: InputDecoration(
+                icon: Icon(
+                  Icons.search,
+                  color: kPrimaryLabelColor,
+                  size: 20.0,
+                ),
+                border: InputBorder.none,
+                hintText: "Search for courses",
+                hintStyle: kSearchPlaceholderStyle,
+              ),
+              style: kSearchTextStyle,
+              onChanged: (newText) {
+                print(newText);
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SidebarButton extends StatelessWidget {
+  const SidebarButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () {
+        print("Sidebar button pressed");
+      },
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      highlightColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      constraints: BoxConstraints(
+        maxWidth: 40.0,
+        maxHeight: 40.0,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(14.0),
+            boxShadow: [
+              BoxShadow(
+                color: kShadowColor,
+                offset: Offset(0, 12),
+                blurRadius: 16.0,
+              )
+            ]),
+        child: Image.asset(
+          'asset/icons/icon-sidebar.png',
+          color: kPrimaryLabelColor,
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 12.0,
+          vertical: 14.0,
         ),
       ),
     );
