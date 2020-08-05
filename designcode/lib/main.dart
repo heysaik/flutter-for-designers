83402bc6967c6779d090870a6c83a44e82c828d6
Sai Kambampati <ssv1065@gmail.com>
2020-08-05 15:05:47 -0700

    8. Stateful Widgets

--- designcode/lib/main.dart
+++ designcode/lib/main.dart
@@ -19,14 +19,95 @@ class MyApp extends StatelessWidget {
             child: Column(
               children: [
                 HomeScreenNavBar(),
-                RecentCourseCard(
-                  course: recentCourses[2],
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        "Recents",
+                        style: kLargeTitleStyle,
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                      ),
+                      Text(
+                        "23 courses, more coming",
+                        style: kSubtitleStyle,
+                      ),
+                    ],
+                  ),
                 ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                RecentCourseList(),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class RecentCourseList extends StatefulWidget {
+  @override
+  _RecentCourseListState createState() => _RecentCourseListState();
+}
+
+class _RecentCourseListState extends State<RecentCourseList> {
+  List<Container> indicators = [];
+  int currentPage = 0;
+
+  Widget updateIndicators() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: recentCourses.map(
+        (course) {
+          var index = recentCourses.indexOf(course);
+          return Container(
+            width: 7.0,
+            height: 7.0,
+            margin: EdgeInsets.symmetric(horizontal: 6.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color:
+                  currentPage == index ? Color(0xFF0971FE) : Color(0xFFA6AEBD),
+            ),
+          );
+        },
+      ).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 320,
+          width: double.infinity,
+          child: PageView.builder(
+            itemBuilder: (context, index) {
+              return Opacity(
+                opacity: currentPage == index ? 1.0 : 0.5,
+                child: RecentCourseCard(
+                  course: recentCourses[index],
+                ),
+              );
+            },
+            itemCount: recentCourses.length,
+            controller: PageController(initialPage: 0, viewportFraction: 0.63),
+            onPageChanged: (index) {
+              setState(() {
+                currentPage = index;
+              });
+            },
+          ),
+        ),
+        updateIndicators(),
+      ],
     );
   }
 }
